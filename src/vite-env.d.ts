7da4e6efbb6c2393f54ef4650f3c2b93552cdd6d
Quincy Zhang <73623026+joygqz@users.noreply.github.com>
2026-07-22 10:01:50 +0800

    fix(sftp): update Monaco imports

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,3 @@
 /// <reference types="vite/client" />
 
 declare const __APP_VERSION__: string;
-
-declare module "monaco-editor/esm/vs/editor/edcore.main" {
-  export * from "monaco-editor";
-}
-
-declare module "monaco-editor/esm/vs/basic-languages/*";
